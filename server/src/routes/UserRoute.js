177ef16b5ea1cc731f01d2c88f3fbb823a723112
ngovanbang2001 <ngovanbang2001@gmail.com>
2023-04-27 22:17:40 +0700

    'refactor code

--- server/src/routes/UserRoute.js
+++ server/src/routes/UserRoute.js
@@ -1,13 +1,12 @@
-import express from 'express';
-import {verifyToken} from "../middleware/auth";
+import express from 'express'
+import { verifyToken } from '../middleware/auth'
 import {
-    createCollaboratorAccount,
-    getAllAccounts,
-    getProfile,
-    getProfileById,
-    lockAccount,
-    updateAccount
-} from "../controllers/UserController";
+  getAllAccounts,
+  getProfile,
+  getProfileById,
+  lockAccount,
+  updateAccount,
+} from '../controllers/UserController'
 
 const router = express.Router()
 
@@ -16,8 +15,5 @@ router.put(`/:id`, verifyToken, updateAccount)
 router.get(`/:id`, getProfileById)
 router.get(`/`, verifyToken, getAllAccounts)
 router.post(`/lock/:id`, verifyToken, lockAccount)
-router.post(`/collaborator`, verifyToken, createCollaboratorAccount)
-
-
 
 export default router
